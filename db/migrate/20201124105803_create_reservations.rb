d05c143b4ced905af2ace8c6d20cd6d67ec53ecb
daichi-fukui <fukui.daichi0913@icloud.com>
2020-11-29 10:37:58 +0900

    application.htmlを編集

--- db/migrate/20201124105803_create_reservations.rb
+++ db/migrate/20201124105803_create_reservations.rb
@@ -1,7 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations do |t|
-
+      t.integer :date_id, null: false
+      t.integer :time_id, null: false
       t.timestamps
     end
   end
